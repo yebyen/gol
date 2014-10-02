0314c70614d512688afa74f6c04855aa9b72bb34
Kingdon Barrett <yebyen@gmail.com>
2014-10-02 13:50:38 -0400

    live neighbors test

--- lib/conways.rb
+++ lib/conways.rb
@@ -1,15 +1,30 @@
 # Start your iteration here
 class Conways
-	def add coordinate
-	end
+  attr_accessor :board
+  def add coordinate
+    board << coordinate
+  end
 
-	def size
-		3
-	end
-	
+  def size
+    3
+  end
+
+  def is_alive? x,y
+    @board.each do|cell|
+      return true if cell.x==x && cell.y==y
+    end
+    return false
+  end
+
+  def initialize
+    @board = []
+  end
 end
 
 class Cell
-	def initialize x, y
-	end
+  attr_accessor :x, :y
+  def initialize x, y
+    @x = x
+    @y = y
+  end
 end
--- spec/conways_spec.rb
+++ spec/conways_spec.rb
@@ -16,10 +16,16 @@ describe "conways" do
   end
 
   it "can tell you if a cell is alive or dead" do
-  	board = Conways.new
-  	board.add(Cell.new(0,0))
-  	expect(board.is_alive?(0,0)).to be true
-  	expect(board.is_alive?(1,1)).to be false
+    board = Conways.new
+    board.add(Cell.new(0,0))
+    expect(board.is_alive?(0,0)).to be true
+    expect(board.is_alive?(1,1)).to be false
   end
 
+  it "can tell you how many live neighbors are around a cell" do
+    board = Conways.new
+    board.add([Cell.new(0,0), Cell.new(1,0), Cell.new(0,1),
+               Cell.new(1,1), Cell.new(-1, -1), Cell.new(-1, 1)])
+    expect(Cell.new(0,0).live_neighbors).to be 5
+  end
 end
