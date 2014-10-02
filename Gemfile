cf98a76f3e6a3f335c0a6f18b3fc00f2d44b6ba2
Kingdon Barrett <yebyen@gmail.com>
2014-10-02 13:23:28 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://www.rubygems.org'
+
+gem 'rspec'
+gem 'pry'
+gem 'awesome_print', :require => 'ap'
---
+++ spec/conways_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'conways'
+
+describe "conways" do
+  it "is a game of life" do
+    false.should be_true
+  end
+end
