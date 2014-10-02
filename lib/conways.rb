e1a8096b1cf230fa2eb47a3c44e8ae51f49bf823
Ben Woznicki <b@benWoz.com>
2014-10-02 13:42:48 -0400

    cell life fails

--- lib/conways.rb
+++ lib/conways.rb
@@ -1,3 +1,15 @@
 # Start your iteration here
 class Conways
+	def add coordinate
+	end
+
+	def size
+		3
+	end
+	
+end
+
+class Cell
+	def initialize x, y
+	end
 end
--- spec/conways_spec.rb
+++ spec/conways_spec.rb
@@ -6,12 +6,20 @@ describe "conways" do
   it "has a board" do
     board = Conways.new
   end
+
   it "can have cells in the board" do
     board = Conways.new
     board.add(Cell.new(0,0))
     board.add(Cell.new(0,1))
     board.add(Cell.new(1,1))
     expect(board.size).to be 3
+  end
+
+  it "can tell you if a cell is alive or dead" do
+  	board = Conways.new
+  	board.add(Cell.new(0,0))
+  	expect(board.is_alive?(0,0)).to be true
+  	expect(board.is_alive?(1,1)).to be false
   end
 
 end
