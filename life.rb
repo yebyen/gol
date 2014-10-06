a14bdf10f6bba686a766f61f538bc79da34c1fcf
Kingdon Barrett <kbarrett@metrixmatrix.com>
2014-10-05 20:22:58 -0400

    it works

--- life.rb
+++ life.rb
@@ -1,6 +1,17 @@
+#!/usr/bin/env ruby
 require './lib/coord'
 require './lib/cell'
 require './lib/board'
 require './lib/move'
 
-
+m = Move.new(Board.new(4,8,"board.txt"))
+c = 0
+while true do
+  system("clear")
+  puts m.board
+  puts
+  puts "Generation: #{c}"
+  sleep 1
+  c += 1
+  m.move
+end
